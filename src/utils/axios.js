6746a2102b11b3e9ed4198eaede03fb868394d68
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-04-15 11:06:20 +0530

    add prod url

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const base = process.env.NODE_ENV === "production"?:"http://localhost:5000"
+const base = process.env.NODE_ENV === "production" ? "https://wms-server-wr3u.onrender.com" : "http://localhost:5000"
 
 const Axios = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: base
 });
 export default Axios
