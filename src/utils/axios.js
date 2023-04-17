6251af10951b9be2e7d1588c652113d0474b4d3d
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-04-17 14:56:27 +0530

    server deploy to cyclish

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const base = process.env.NODE_ENV === "production" ? "https://wms-server-wr3u.onrender.com" : "http://localhost:5000"
+//url = https://tiny-jade-goshawk-belt.cyclic.app
+//url1 = https://wms-server-wr3u.onrender.com
+
+const base = process.env.NODE_ENV === "production" ? "https://tiny-jade-goshawk-belt.cyclic.app" : "http://localhost:5000"
 
 const Axios = axios.create({
     baseURL: base
