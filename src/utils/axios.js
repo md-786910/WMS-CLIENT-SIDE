761935cf3505a277d57cb5e28a42a4b303629604
md ashif reza <76996887+md-786910@users.noreply.github.com>
2023-05-09 11:40:43 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //url = https://tiny-jade-goshawk-belt.cyclic.app
 //url1 = https://wms-server-wr3u.onrender.com
 
-const base = process.env.NODE_ENV !== "production" ? "https://tiny-jade-goshawk-belt.cyclic.app" : "http://localhost:5000"
+const base = process.env.NODE_ENV === "production" ? "https://tiny-jade-goshawk-belt.cyclic.app" : "http://localhost:5000"
 
 const Axios = axios.create({
     baseURL: base
