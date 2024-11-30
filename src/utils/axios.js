1566da134560b87a0fd18b5b8c6106acb720fe44
md ashif reza <76996887+md-786910@users.noreply.github.com>
2024-11-30 10:48:18 +0530

    Update axios.js

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 //url1 = https://wms-server-wr3u.onrender.com
-
+//"https://d3ckitf346p739.cloudfront.net"
+const render_url = "https://wms-server-wr3u.onrender.com";
 const base =
   process.env.NODE_ENV === "production"
-    ? "https://d3ckitf346p739.cloudfront.net"
+    ? render_url
     : "http://localhost:5000";
 
 const Axios = axios.create({
