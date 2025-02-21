6d7a311c1fea99b279cc8aa3a1b5b4d6cfd0a334
md-786910 <mdashifreza111@gmail.com>
2025-02-21 14:25:23 +0530

    update API!

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,7 +5,7 @@ import axios from "axios";
 const render_url = "https://wms-server-wr3u.onrender.com";
 const base =
   process.env.NODE_ENV === "production"
-    ? "https://wms-1uq0kpc6g-md786910s-projects.vercel.app/"
+    ? "https://wmsserver.vercel.app"
     : "http://localhost:5000";
 
 const Axios = axios.create({
